b641407205d3832667149d7105a8f41f6368f366
JoaoVitorFigueiredo <jvlfo@iscte-iul.pt>
2025-04-14 19:05:39 +0100

    algumas mudanças

--- echoes-of-choice/scripts/player.gd
+++ echoes-of-choice/scripts/player.gd
@@ -7,7 +7,23 @@ func _physics_process(delta):
 	player_movement(delta)
 
 func player_movement(delta):
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_up"):
+		velocity.x = speed
+		velocity.y = -speed
+		
+	elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down"):
+		velocity.x = speed
+		velocity.y = speed
+	
+	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down"):
+		velocity.x = -speed
+		velocity.y = speed
+		
+	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_up"):
+		velocity.x = -speed
+		velocity.y = -speed
+	
+	elif Input.is_action_pressed("ui_right"):
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
