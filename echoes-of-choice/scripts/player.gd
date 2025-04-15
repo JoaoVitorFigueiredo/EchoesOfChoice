8ec7a2d17ba40dbec7b5b7f4c4f916a39be284d2
JoaoVitorFigueiredo <jvlfo@iscte-iul.pt>
2025-04-15 15:56:22 +0100

    Vila e caverna(n tem nada)

--- echoes-of-choice/scripts/player.gd
+++ echoes-of-choice/scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var curr_dir = "none"
 var speed =100
 
 func _physics_process(delta):
@@ -8,36 +8,70 @@ func _physics_process(delta):
 
 func player_movement(delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_up"):
+		curr_dir = "right"
+		play_animation(1)
 		velocity.x = speed
 		velocity.y = -speed
 		
 	elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down"):
+		curr_dir = "right"
+		play_animation(1)
 		velocity.x = speed
 		velocity.y = speed
 	
 	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down"):
+		curr_dir = "left"
+		play_animation(1)
 		velocity.x = -speed
 		velocity.y = speed
 		
 	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_up"):
+		curr_dir = "left"
+		play_animation(1)
 		velocity.x = -speed
 		velocity.y = -speed
-	
+		
 	elif Input.is_action_pressed("ui_right"):
+		curr_dir = "right"
+		play_animation(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		curr_dir = "left"
+		play_animation(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
+		play_animation(1)
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
+		play_animation(1)
 		velocity.x = 0
 		velocity.y = -speed
 	
 	else:
+		play_animation(0)
 		velocity.x = 0
 		velocity.y = 0
 		
 	move_and_slide()
+
+
+func play_animation(movement):
+	var dir = curr_dir
+	var anim =$AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walk")
+		elif movement == 0:
+			anim.play("idle")
+			
+	else:
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("walk")
+		elif movement == 0:
+			anim.play("idle")
