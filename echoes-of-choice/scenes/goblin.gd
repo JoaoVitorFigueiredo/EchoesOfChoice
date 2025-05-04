9fce0abe3e9b7e82d5785b52b63700d903ee377f
JoaoVitorFigueiredo <jvlfo@iscte-iul.pt>
2025-05-05 00:06:07 +0100

    Joovi | Já se mexem nessa desgraça
    td fdd mas já andam

--- echoes-of-choice/scenes/goblin.gd
+++ echoes-of-choice/scenes/goblin.gd
@@ -3,10 +3,20 @@ extends PathFollow2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
+var going_back = false
 
+@onready var goblin = $goblin
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $goblin.action = 0
-	progress += 4 * delta
+	print(goblin.action)
+	if goblin.action == 0:
+		if progress_ratio == 1 or progress_ratio == 0:
+			going_back = !going_back
+		if going_back:
+			progress -= 25 * delta
+		else:
+			progress +=25 * delta
+		
+		
