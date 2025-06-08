7af94940724d4ad1a1b6b55a5b1ecf93a5fbdf0e
David Manuel Matias Isaac <131678864+David-M-M-Isaac@users.noreply.github.com>
2025-06-08 17:39:13 +0100

    retirar restart

--- echoes-of-choice/scenes/pause_menu.gd
+++ echoes-of-choice/scenes/pause_menu.gd
@@ -6,10 +6,5 @@ extends Control
 func _on_continuar_pressed() -> void:
 	main.pauseMenu()
 
-
-func _on_recomeçar_pressed() -> void:
-	var current_scene: String = Global.current_scene
-	get_tree().change_scene_to_file(current_scene)
-
 func _on_sair_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/control.tscn")
